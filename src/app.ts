24550c5d91ee686cbd92a30e9abfbca355532121
ranmo.cy <ranmo.cy@alibaba-inc.com>
2021-03-18 16:25:35 +0800

    add highlight

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+// src/app.ts
+import Prism from 'prism-react-renderer/prism';
+
+(typeof global !== 'undefined' ? global : window).Prism = Prism;
+
+require('prismjs/components/prism-kotlin');
+require('prismjs/components/prism-dart');
