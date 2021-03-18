5b73c3d785469f982e8faafcf231e28fbc0ee1fe
ranmo.cy <ranmo.cy@alibaba-inc.com>
2021-03-18 16:29:21 +0800

    del kotlin

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,4 @@ import Prism from 'prism-react-renderer/prism';
 
 (typeof global !== 'undefined' ? global : window).Prism = Prism;
 
-require('prismjs/components/prism-kotlin');
 require('prismjs/components/prism-dart');
