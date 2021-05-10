c0dfacee930ebeeec37973da90f3e182dde89101
染陌同学 <ranmo.cy@alibaba-inc.com>
2021-05-10 18:40:24 +0800

    fix: del dark mode (#133)

--- .umirc.ts
+++ .umirc.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   favicon:
     'https://img.alicdn.com/imgextra/i1/O1CN01u0y8XT25HxxAVpBQL_!!6000000007502-2-tps-109-103.png',
   styles: [
+    '__dumi-default-dark: { display: none }',
     '@keyframes levitate { 0% { transform: translate3d(0,-2.3%,0);} 50% {transform: translate3d(0,2.3%,0);} 100% {transform: translate3d(0,-2.3%,0);}}',
     '.img-animation{ animation: levitate 5s ease-in-out infinite forwards }',
     '.__dumi-default-code-block{ background-color: #F9F9F9 }',
