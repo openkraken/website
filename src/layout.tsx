d199c203a5f14172baace70e3eaa5001eda3ac42
卓凌 <zhuoling.lcl@alibaba-inc.com>
2021-09-17 11:18:19 +0800

    docs: police record

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,13 +1,9 @@
 import Layout from 'dumi-theme-default/src/layout';
 
 export default ({ children, ...props }) => {
-  console.log(133424324321321);
   return (
     <Layout {...props}>
-      <>
-        aaaaaa
-        {children}
-      </>
+      <>{children}</>
     </Layout>
   );
 };
