36941467aa3a37b21e4f13cc8cf647cd098e71f7
任跃兵 <cnryb@live.cn>
2022-03-08 14:17:29 +0800

    根据浏览器语言切换网站语言 (#201)

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,10 @@ import Prism from 'prism-react-renderer/prism';
 require('prismjs/components/prism-dart');
 require('prismjs/components/prism-swift');
 require('prismjs/components/prism-kotlin');
+
+if (window && navigator && window.location.pathname === '/') {
+  var lang = navigator.language;
+  if (lang !== 'zh-CN') {
+    window.location.href = '/en-US';
+  }
+}
